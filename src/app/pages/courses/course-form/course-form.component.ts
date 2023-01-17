a739d3fc7baee2e12a966efcf61a7daa14766614
Ahmad Altayeh <ahmadaltayeh31@gmail.com>
2023-01-17 08:46:40 +0300

    Change classrooms

--- src/app/pages/courses/course-form/course-form.component.ts
+++ src/app/pages/courses/course-form/course-form.component.ts
@@ -15,7 +15,7 @@ export class CourseFormComponent implements OnInit {
   times = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00'];
   smstr: string[] = ['1st', '2nd'];
   blean: boolean[] = [true, false];
-  classroms: string[] = ['classroom 01', 'classroom 02'];
+  classroms: string[] = ['القاعة الهاشمية', 'قاعة الامام الغزالي', "قاعة الامام البخاري", "قاعة الامام أحمد بن حنبل", "قاعة الامام الشافعي"];
   form: FormGroup
 
 
